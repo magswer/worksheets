3e4a0c19d9140f22116a2d3a988611fb231e262f
Magda Wernik <ma.wernik@gmail.com>
2025-02-18 20:57:00 +0100

    add assignments and student worksheets tables

---
+++ database/migrations/2025_02_18_192508_create_student_worksheets_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+   
+    public function up(): void
+    {
+        Schema::create('student_worksheets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id');
+            $table->foreignId('worksheet_id');
+            $table->boolean('is_completed')->default(false);
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_worksheets');
+    }
+};
